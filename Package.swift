d3f3f0d158d224145ece6869bdc59800f1670246
Robert M. Lefkowitz <r0ml@mac.com>
2024-02-29 17:50:06 -0500

    updated version to 0.2.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
     //  .binaryTarget(name: "ffi", path: "libffi.xcframework"),
       .binaryTarget(name: "ffi",
-                    url: "https://github.com/r0ml/FFI-Framework/releases/download/0.1/libffi.xcframework.zip",
+                    url: "https://github.com/r0ml/FFI-Framework/releases/download/0.2.0/libffi.xcframework.zip",
                     checksum: "d55719198d2104b9439537b391381e98e6383f75af2caf4b0a3efd2076066724"
                    )
     ]
