7d8b494ba54adc5a5d564a4dc5e66f2fc05ffeaf
Robert M. Lefkowitz <r0ml@mac.com>
2024-02-29 16:06:06 -0500

    initial import

--- Package.swift
+++ Package.swift
@@ -14,10 +14,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ffi"),
-        .testTarget(
-            name: "ffiTests",
-            dependencies: ["ffi"]),
+      .binaryTarget(name: "ffi", path: "libffi.xcframework")
     ]
 )
